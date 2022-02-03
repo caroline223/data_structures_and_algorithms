403265bc4d4966bea11e0df856e387c523ad8fc3
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-03 01:36:35 -0500

    Wrote the solution to the find_shortest_string.js file

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,5 @@
 function findShortestString(arr) {
-  // type your code here
+  return arr.sort((a, b) => a.length - b.length)[0]
 }
 
 if (require.main === module) {
@@ -24,3 +24,10 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//Iterating through each string within the array, you are looking to determine the string with the shortest 
+//length. According to the code, if the difference between the length of the first string and the length
+//of the second string is positive, that means that the second string is shorter than the first string and should
+//be moved to the beginning index of the array. Likewise, if the difference is negative, that means that the length of
+//the first string is shorter than the length of the second string and should remain as the beginning index 
+//of the array. 
