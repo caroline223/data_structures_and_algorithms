dbd04dcbdd3ccaddf871464e75c9e9349c7a88a4
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-03 01:40:50 -0500

    Wrote the solution to the recursive_count.js file

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,9 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if(num > 10){
+    return;
+  }
+  console.log(num)
+  recursiveCount(num + 1)
 }
 
 if (require.main === module) {
@@ -11,3 +15,7 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//The idea of this challenge is that you want a function to call onto itself so that when from numbers 0 - 9, 
+//it would output the number
+//However, once the number reaches 10, the function stops
