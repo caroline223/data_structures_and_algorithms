73d3e8658574faa5513ecc0dfb26d15ebfeca184
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-03 01:26:57 -0500

    Wrote the solution to the find_first_duplicate.js file

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,12 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let elementSet = new Set()
+
+  for(let i = 0; i < arr.length; i++){
+    if(elementSet.has(arr[i])) return arr[i]
+    elementSet.add(arr[i])
+  }
+  return -1 
 }
 
 if (require.main === module) {
@@ -17,3 +24,12 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// For this code, I assgined a variable elementSet to be a new Set of unique values that 
+// were originally derived from a given array. I then established a loop for which the loop was to iterate
+// through each element of a given array and if the number was not the fist instance of a duplicate, enter 
+// that number into the new set for which that said number is given a unqiue identifier. However, if a number appears as a duplicate,
+// the loop would stop and return the value of the duplicate number. It would not be added to the set . 
+// If after iterating through the loop and there are no duplicates, it would return a value of -1. 
+//Note: every value in the Set() has to be unique; there cannot be any duplicate values. 
+
