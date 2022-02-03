12b2e24e84525d7ed673b6d7d80c821eafa2884a
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-03 01:33:05 -0500

    Wrote the solution to the selection_sort.js file

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,6 +1,21 @@
 function selectionSort(arr) {
   // type your code here
+  for(let i = 0; i < arr.length - 1; i++){
+    let minIndex = i
+    for(let j = i + 1; j < arr.length; j++ ){
+      if(arr[minIndex] > arr[j]){
+        minIndex = j
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+  }
+  return arr
 }
+
+//You have to create a for loop where it would iterate through an array for a set number of times
+//(depending on the length of the array) to then first find the smallest number. Once it finds the smallest
+//number, the loop would set it as the minimum number and place said number in the front of the array.
+//The process would continue until all numbers have been sorted 
 
 if (require.main === module) {
   // add your own tests in here
