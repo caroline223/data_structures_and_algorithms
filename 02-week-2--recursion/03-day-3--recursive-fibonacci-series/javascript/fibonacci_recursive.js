fc1dca8a625e281906a2222ea52ac8efcdf7ab00
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-08 01:07:27 -0500

    Wrote the solution to the fibonacci_recursive.js file

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -1,5 +1,8 @@
 function fibonacci(n) {
   // type your code here
+  if (n <=1 ) return n
+
+  return fibonacci(n -1) + fibonacci(n - 2)
 }
 
 if (require.main === module) {
@@ -22,3 +25,8 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//The idea behind my solution is that the first two numbers in a fibonacci series are 0 and 1. So, in this case,
+//if the value of n is less than or equal to 1, then return the value of n itself. Otherwise, we would return the value 
+// of n within the fibonacci series as the sum of the preceeding two numbers in the series which would give you the value of the 
+// next number. 
