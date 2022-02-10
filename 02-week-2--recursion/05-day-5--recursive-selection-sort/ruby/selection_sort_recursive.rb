f58a1e3c26747d389b54be5f4fbc0548b0f99181
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-10 01:33:37 -0500

    Wrote the solution to the selection_sort_recursive.rb file

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
@@ -1,5 +1,16 @@
 def selection_sort_recursive(arr)
   # type your code in here
+  return [] if arr.length == 0 #base case
+
+  min_value = arr.min #splat operator 
+  index_value = arr.index(min_value)
+  arr.delete_at(index_value)
+  
+
+  result = selection_sort_recursive(arr)
+  result.unshift(min_value)
+
+   
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -9,6 +20,9 @@ if __FILE__ == $PROGRAM_NAME
 
   puts
 
+  puts "Expecting: [-8, -2, 4, 6]"
+  print "=> "
+  print selection_sort_recursive([4, -8, -2, 6])
   # Don't forget to add your own!
 end
 
