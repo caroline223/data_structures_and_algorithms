9ce205f341c1b4f3274ee34e5f6aa2e553f2ca64
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-10 01:12:31 -0500

    Wrote the solution to the selection_sort_recursive.js file

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,15 @@
 function selectionSortRecursive(arr) {
   // type your code here
+ if (arr.length === 0) return [] //base case
+
+ const minValue = Math.min(...arr)
+ const index = arr.indexOf(minValue)
+ arr.splice(index, 1)
+
+ const result = selectionSortRecursive(arr)
+ result.unshift(minValue)
+
+ return result 
 }
 
 if (require.main === module) {
@@ -14,3 +24,24 @@ module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//Iterative Solution
+
+//function selection_sort_recursive(arr){
+
+//for(let i = 0; i < arr.length - 1; i++){
+
+  // let minIndex = i
+  // for (let j = i + 1; j < arr.length; j++){
+
+    // if( arr[j] < arr[minIndex]){
+
+      // minIndex = j
+    // }
+  // }
+  // [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+// }
+  // return arr
+//}
+
+
