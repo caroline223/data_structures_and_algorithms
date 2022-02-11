11b9faef272aa90dffdf9be3c43470ab94449acb
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-10 23:11:11 -0500

    Wrote my write up for my solution to the balancing_parentheses.js file

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -37,3 +37,14 @@ module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//First, I established an empty array which will hold the group of parentheses
+//Then, I iterated with a for loop for which the initial logic states that if the first element of the array
+//starts with the left parenthesis, then push that parenthesis to the end of the array to balance out.
+
+//Then, I included the logic that if the first element of the array starts with the right parenthesis, then,
+//first look to see if the ending of the array has the left parenthesis. If it does, then remove it from the array
+//so that it can balance out. If not, then add the appropriate number of right parentheses to the end of the 
+//array to balance the array out. 
+
+//Lastly, return the number of missing parentheses needed to add to the array. 
