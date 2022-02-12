6f5604c7fc2f4d5d6453d846bfa1dfc1dd7460e1
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-12 15:00:16 -0500

    Wrote the solution to the roman_numeral.js file

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,47 @@
 function romanNumeral(string) {
   // type your code here
+  const romanLetter = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+
+  let accumulator = 0
+
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === "I" && string[i + 1] === "V"){
+      accumulator += 4
+      i++;
+    }
+    else if(string[i] === "I" && string[i + 1] === "X"){
+      accumulator += 9
+      i++;
+    }
+    else if(string[i] === "X" && string[i + 1] === "L"){
+      accumulator += 40
+      i++;
+    }
+    else if(string[i] === "X" && string[i + 1] === "C"){
+      accumulator += 90
+      i++;
+    }
+    else if(string[i] === "C" && string[i + 1] === "D"){
+      accumulator += 400
+      i++;
+    }
+    else if(string[i] === "C" && string[i + 1] === "M"){
+      accumulator += 900
+      i++;
+    }
+    else {
+      accumulator += romanLetter[string[i]]
+    }
+  }
+  return accumulator
 }
 
 if (require.main === module) {
