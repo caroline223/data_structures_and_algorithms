014a69c3dcfa4114187fe6f53376912e8e898ad3
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-12 19:39:29 -0500

    Wrote the solution to the roman_numeral.rb file

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
@@ -1,6 +1,40 @@
 def roman_numeral(string)
   # type your code in here
+  roman_letter = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  }
+
+  index = 0
+  accumulator = 0
+   
+  while index < string.length 
+    two_characters = (string[index] + (string[index + 1] || '')).to_sym
+
+    if !roman_letter[two_characters].nil?
+      accumulator += roman_letter[two_characters]
+      index += 2
+    else
+      accumulator += roman_letter[string[index].to_sym]
+      index += 1
+    end
+  end
+  
+ accumulator
 end
+
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 1"
