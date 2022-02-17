d8b164dfc3ca5377ad25317915bec028848f6fe5
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-17 01:29:33 -0500

    Wrote the solution to the distinct_pair_sum.rb file

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/ruby/distinct_pair_sum.rb
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/ruby/distinct_pair_sum.rb
@@ -1,5 +1,17 @@
 def distinct_pair_sum(arr, k)
   # type your code in here
+
+  set_pairs = {}
+
+ (0...(arr.length - 1)).each do |index|
+  next_value = arr[index + 1]
+  current_value = arr[index]
+  if current_value + next_value == k && set_pairs[current_value] == nil && set_pairs[next_value] == nil
+    set_pairs[current_value] = [current_value, next_value]
+  end
+end
+
+ set_pairs.values 
 end
 
 if __FILE__ == $PROGRAM_NAME
