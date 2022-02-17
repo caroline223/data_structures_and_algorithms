f57a14b02a618ff156c731ad2ba9af3d089a7b84
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-17 01:15:09 -0500

    Wrote the solution to the distinct_pair_sum.js file

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -1,5 +1,15 @@
 function distinctPairSum(arr, k) {
   // type your code here
+  const setPairs = {}
+
+  arr.slice(0, -1).forEach((number, index) => { // starting from the first index and going up to the second-to-last index
+    const nextValue = arr[index + 1]
+    if(number + nextValue === k && setPairs[number] === undefined && setPairs[nextValue] === undefined){
+        setPairs[number] = [number, nextValue]
+    }
+  });
+
+  return Object.values(setPairs) // Here is where we return the unique paired values
 }
 
 if (require.main === module) {
@@ -17,3 +27,15 @@ module.exports = distinctPairSum;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//When iterating over the array, we are comparing if the current value and the next value add up to k.
+//We are also looking to ensure that although there may be duplicates within the array, when forming the pairs, 
+//that they are distinct pairs that add up to the given k value. 
+
+//Objects allows for keys with unique values, which it what we are looking for. 
+//We establish the variable setPairs as an empty hash for which the key-value pairs would be stored
+//So, we first have to slice the array from the first index to the second-to-last index
+//Then check if the current value and the next value add up to k. If that is true, we want to ensure that 
+// both numbers are not already used as keys in another key-value pair. So long as that is true,
+//we would set the current value as the key and the nextValue as the value to establish the key-value pair
+//for the given k value. 
