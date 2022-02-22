84eef5e1195f00a6a321f9b2682b12d9a65c2cf0
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-21 20:00:03 -0500

    Updated the solution to the fibonacci_recursive_series.js file

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -1,8 +1,8 @@
 function fibonacci(n) {
   // type your code here
-  if (n <=1 ) return n
+  if(n <= 1) return n
 
-  return fibonacci(n -1) + fibonacci(n - 2)
+  return fibonacci(n -2) + fibonacci(n - 1)
 }
 
 if (require.main === module) {
