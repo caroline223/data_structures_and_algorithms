a847bb642c3ff5059a42a0e311f52ea2b621e02c
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-21 19:54:27 -0500

    Updated the solution to balancing_parentheses.js file

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -1,20 +1,21 @@
 function balancingParentheses(string) {
   // type your code here
-  let parentheses = []
+ let parentheses = []
 
-  for(let i = 0; i < string.length; i++){
-    if(string[i] === "("){
-      parentheses.push(string[i])
-    }
-    else if (string[i] === ")"){
+ for(let i = 0; i < string.length; i++){
+   if(string[i] === "("){
+     parentheses.push(string[i])
+   }
+   else if (string[i] === ")"){
      if(parentheses[parentheses.length - 1] === "("){
-       parentheses.pop();
-     } else{
+       parentheses.pop()
+     }
+     else{
        parentheses.push("#")
      }
-    }
-  }
-  return parentheses.length
+   }
+ }
+ return parentheses.length
 }
 
 if (require.main === module) {
