8d08c5fb34856c8ca8626b35b06d87a1bb41375f
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-21 19:50:02 -0500

    Updated solution

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,10 +1,10 @@
 function rotateArray(arr, k) {
   // type your code here
+  const rotations = k % arr.length
 
-  const numberOfRotations = k % arr.length;
-  const removed = arr.splice(arr.length - numberOfRotations, numberOfRotations);
+  const removed = arr.splice(arr.length - rotations, rotations)
 
-  return removed.concat(arr);
+  return removed.concat(arr)
 }
 
 if (require.main === module) {
@@ -30,3 +30,5 @@ module.exports = rotateArray;
 
 //For this problem, my initial thought was that in order to have a rotating array, we would need to unshift the first element in the array
 // to then take that same element and push it to the end of the array a set number of times (in accordance to the number of rotations assigned)
+
+//We need to take into account when k is the same or larger than the array's length by getting the remainder of k / arr.length
