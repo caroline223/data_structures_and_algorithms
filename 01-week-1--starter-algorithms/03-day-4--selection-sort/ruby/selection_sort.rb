acfbee1afad0469f4ccef641ae13670b7131e378
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-24 00:50:21 -0500

    Rewrote solution to the selection_sort.rb file

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
@@ -1,20 +1,27 @@
 def selection_sort(arr)
-  n = arr.length - 1
 
-  n.times do |i|
-    min_index = i 
-    for j in (i + 1)..n 
-      min_index = j if arr[min_index] > arr[j]
+  n = arr.length - 1
+ 
+    n.times do |i|
+      min_index = i
+      for j in (i + 1)..n 
+        min_index = j if arr[j] < arr[min_index]
+      end
+      arr[i], arr[min_index] = arr[min_index], arr[i]
     end
-    arr[i], arr[min_index] = arr[min_index], arr[i]
-  end
- return arr
+  return arr
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [-1, 2, 3, 5]"
   print "=> "
   print selection_sort([3, -1, 5, 2])
+
+  puts
+
+  puts "Expecting: [-8, -4 , 3, 12]"
+  print "=> "
+  print selection_sort([12, -4, 3, -8])
 
   puts
 
