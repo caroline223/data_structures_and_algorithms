95d038b96156e9f2900f696e1ad5150f83ac928a
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-02-24 00:41:35 -0500

    Rewrote solution to the selection_sort.js file

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,13 +1,13 @@
 function selectionSort(arr) {
   // type your code here
   for(let i = 0; i < arr.length - 1; i++){
-    let minIndex = i
-    for(let j = i + 1; j < arr.length; j++ ){
-      if(arr[minIndex] > arr[j]){
-        minIndex = j
+      let minIndex = i
+      for(let j = i + 1; j < arr.length; j++){
+        if(arr[j] < arr[minIndex]){
+          minIndex = j
+        }
       }
-    }
-    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+      [arr[i], arr[minIndex]] =[arr[minIndex], arr[i]]
   }
   return arr
 }
