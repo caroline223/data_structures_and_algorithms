0257787974b846130a48d082017aa076def2a501
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-04-10 18:40:13 -0400

    Update

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
@@ -1,19 +1,19 @@
 function consecutiveSubstrings(string) {
   // type your code here
-  const newArr = string.split('')
-  const subStrings = []
+ const newArr = string.split('')
+ const subStrings = []
 
-  newArr.forEach((character, index) => {
-    subStrings.push(character)
+ newArr.forEach((character, index) => {
+   subStrings.push(character)
 
-    let fragment = character
+   let fragment = character 
 
-    newArr.slice(index + 1).forEach((letter) => {
-      fragment += letter
-      subStrings.push(fragment)
-    })
-  })
-  return subStrings
+   newArr.slice(index + 1).forEach((letter) => {
+     fragment += letter
+     subStrings.push(fragment)
+   })
+ })
+ return subStrings
 }
 
 if (require.main === module) {
