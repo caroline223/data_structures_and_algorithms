6ada33e25e35f78286d457dd69ae9ca6a299ed40
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-04-10 18:48:22 -0400

    Wrote the solution to the consective_substrings.rb file

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/ruby/consecutive_substrings.rb
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/ruby/consecutive_substrings.rb
@@ -1,5 +1,22 @@
 def consecutive_substrings(string)
   # type your code in here
+  new_arr = string.split('')
+  sub_strings = []
+
+  new_arr.each_with_index do |character, index|
+
+    sub_strings << character 
+
+    fragment = character 
+
+    new_arr.slice(index + 1 .. -1).each_with_index do |letter|
+        fragment += letter
+        sub_strings << fragment
+    end
+
+  end
+  sub_strings
+
 end
 
 if __FILE__ == $PROGRAM_NAME
